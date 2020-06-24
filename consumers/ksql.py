b1a75af4d7c9ebba2dd00b8ed008a1d9430c571b
Alex Schaeper <alex@schaeper.iO>
2020-06-24 10:15:48 +0000

    Add KSQL tables for turnstile consolidation

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -1,6 +1,7 @@
 """Configures KSQL to combine station and turnstile data"""
 import json
 import logging
+from pathlib import Path
 
 import requests
 
@@ -9,28 +10,31 @@ import topic_check
 
 logger = logging.getLogger(__name__)
 
+with open(f"{Path(__file__).parents[0]}/../conf.json", "r") as fd:
+    conf = json.load(fd)
 
-KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
+KSQL_URL = conf["kafka"]["ksql"]["url"]
+
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT, 
+    station_name VARCHAR, 
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC='cta.turnstiles', 
+    VALUE_FORMAT='avro', 
+    KEY='station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT='json') AS
+    SELECT 
+        station_id, 
+        count(*) AS count 
+    FROM turnstile 
+    GROUP BY station_id;
 """
 
 
